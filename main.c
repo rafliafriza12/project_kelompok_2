d6987953ed3824ba826eac4e5b721b4e34ea5070
Agil Mughni <127282586+agil00@users.noreply.github.com>
2023-05-17 23:09:32 +0700

    menggabungkan file countSizeBinary dengan main

--- main.c
+++ main.c
@@ -52,6 +52,28 @@ fclose(fp);
 }
 
 
+int countSizeBinary(int size,regist login1){
+    FILE * bin;
+    bin = fopen("./assets/login.bin","rb");
+    if(bin==NULL){
+        printf("Tidak Bisa Membuka FIle");
+        exit(1);
+    }
+
+    fseek(bin,0,SEEK_END);
+    long int ukuran = ftell(bin);
+    rewind(bin);
+
+    size = ukuran/sizeof(login1);
+
+    fclose(bin);
+
+    return (size);
+}
+
+
+
+
 
 int login(regist login2[],int size,char * username,char * password){
     int result;
