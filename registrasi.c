9b3f3211f751d10b442b0cd45cde754a0dae6651
Agil Mughni <127282586+agil00@users.noreply.github.com>
2023-05-17 19:34:09 +0700

    update registrasi.c

--- registrasi.c
+++ registrasi.c
@@ -5,7 +5,7 @@
 
 void registrasi(signUp regis){
 FILE *fp ;
-fp = fopen("./Database/login.bin","ab");
+fp = fopen("./database/login.bin","ab");
 
 if(fp== NULL){
     printf("Error opening file\n");
